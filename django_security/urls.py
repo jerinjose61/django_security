205b7f5cb25081b612b94db71daaf9da95386d89
Jerin Jose <jerinjose61@gmail.com>
2021-05-04 21:23:07 +0530

    Change Password

--- django_security/urls.py
+++ django_security/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('change_password', auth_views.PasswordChangeView.as_view(), name='change_password'),
+    path('password_change_done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('',include('app.urls')),
 ]
